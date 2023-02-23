d9db9468faa48bfa9fcfc880279764aea9320768
Thabile22 <Thabilektk@gmail.com>
2023-02-22 17:14:49 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description - Print the absolute value of an integer
+ * @n: Integer of abs value
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
