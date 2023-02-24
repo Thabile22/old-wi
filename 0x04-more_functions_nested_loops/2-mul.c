46153759dea2e6de93af2874646fff57b96832a6
Thabile22 <Thabilektk@gmail.com>
2023-02-24 03:55:03 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description - Multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
