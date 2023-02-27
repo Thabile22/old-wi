4b56ee052c2ef51dfbeb6703ce50134966e7b470
Thabile22 <Thabilektk@gmail.com>
2023-02-27 02:59:24 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * Description: Adding two integers
+ * @i: First int
+ * @j: Second int
+ * Return: int
+ */
+
+int add(int i, int j)
+{
+	int ans = i + j;
+
+	return (ans);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-9-times_table.c
+add.c
