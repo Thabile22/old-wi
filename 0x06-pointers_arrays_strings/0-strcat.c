958521f54a6caad90e6a14b820a5626fb3bf98dc
Thabile22 <Thabilektk@gmail.com>
2023-03-02 01:19:19 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - entry point
+ * Description - Concatenates two strings
+ * @dest: 1st character
+ * @src: 2nd character
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = '\0';
+	return (dest);
+}
