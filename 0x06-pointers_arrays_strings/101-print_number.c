c4770c2da5c453da3a92b50b7fed95f65e51e06d
Thabile22 <Thabilektk@gmail.com>
2023-03-05 05:00:23 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - entry point
+ * Description - Print an integer
+ * @n: input value
+ * Return: int
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar((x % 10) + '0');
+}
