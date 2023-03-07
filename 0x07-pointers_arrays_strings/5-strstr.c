68eec7e14e8c688f596f721f7e49c493acdd788a
Thabile22 <Thabilektk@gmail.com>
2023-03-07 15:45:19 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strstr - entry point
+ * Description - Locate a substring
+ * @haystack: 1st input value
+ * @needle: 2nd input value
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
