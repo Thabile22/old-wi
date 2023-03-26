d4b972a009e6876d2090b8c542b51affda6bbde4
Thabile22 <Thabilektk@gmail.com>
2023-03-26 16:13:29 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 
-	while (n < len2 && i < (len1 + n))
+	while (n >= len2 && i < (len1 + n))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
